984414aa148777fc8d350eb211edd6661007355c
benarba mohammed <tewfikhotspur@gmail.com>
2024-10-06 21:05:03 +0100

    create the upper half of the todo list and style it

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
+import { RxRocket } from "react-icons/rx";
+
 export default function Home() {
-  return <div>todo</div>;
+  return (
+    <section>
+      {/* title */}
+      <div className="bg-gray700 h-[20vh] flex justify-center items-center">
+        <h1 className="text-darkPurple text-center text-2xl font-bold flex justify-center items-center">
+          <RxRocket className="text-blue" />
+          <span className="text-blue ml-2">to</span>do
+        </h1>
+      </div>
+    </section>
+  );
 }
