64cacde029a3766a0346cbf7539185f17816284c
benarba mohammed <tewfikhotspur@gmail.com>
2024-10-07 21:11:14 +0100

    add specific collors for the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        darkblue: "#1e6f9f",
+        blue: "#4ea8de",
+        darkPurple: "#5e60ce",
+        purple: "8284fa",
+        gray700: "#0d0d0d",
+        gray600: "#1a1a1a",
+        gray500: "#262626",
+        gray400: "#333333",
+        gray300: "#808080",
+        gray200: "#d9d9d9",
+        gray100: "#f2f2f2",
+        danger: "#e25858",
       },
     },
   },
