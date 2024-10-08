797dc748c45f7ee27fab572233e0bc2d1f5859be
benarba mohammed <tewfikhotspur@gmail.com>
2024-10-08 19:37:21 +0100

    add custom intrer font to be the font the app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import { RxRocket } from "react-icons/rx";
-
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
-    <section>
+    <section className={inter.className}>
       {/* title */}
       <div className="bg-gray700 h-[20vh] flex justify-center items-center">
         <h1 className="text-darkPurple text-center text-2xl font-bold flex justify-center items-center">
