0440ecbfa3ed0374bac9a9bf19a3a4d209a40c35
benarba mohammed <tewfikhotspur@gmail.com>
2024-10-09 21:13:54 +0100

    center the form in the middle of the screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
-    <section className={`${inter.className} relative`}>
+    <section className={`${inter.className} bg-gray600`}>
       {/* title */}
       <div className="bg-gray700 h-[200px] flex justify-center items-center">
         <h1 className="text-darkPurple text-center text-[40px] font-black flex justify-center items-center">
@@ -14,15 +14,18 @@ export default function Home() {
         </h1>
       </div>
       {/* list */}
-      <div>
+      <div
+        className="w-[90vw] max-w-[736px] mx-auto relative"
+        style={{ height: "calc(100vh - 200px)" }}
+      >
         {/* form */}
-        <form className="absolute left-1/2 -translate-x-1/2 -bottom-6 w-[90vw] max-w-[736px]">
+        <form className="absolute left-1/2 -translate-x-1/2 -top-7 w-full flex justify-center items-center gap-3">
           <input
             type="text"
             placeHolder="add a new task"
-            className="bg-gray500 h-[54px] p-3 text-gray300 capitalize border border-gray700"
+            className="bg-gray500 h-[54px] p-3 text-gray300 grow capitalize border border-gray700"
           />
-          <button className="bg-darkblue h-[54px] text-white px-3 text-[14px] flex justify-center items-center gap-1">
+          <button className="bg-darkblue h-[54px] text-white px-3 basis-[90px] text-[14px] flex justify-center items-center gap-1 focus-visible:outline-none">
             create
             <FiPlusCircle />
           </button>
