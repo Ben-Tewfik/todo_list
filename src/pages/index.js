300e1afed08b88f24ae99b7df598cc7558429aca
benarba mohammed <tewfikhotspur@gmail.com>
2024-10-11 22:10:11 +0100

    change the max with for the paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
         {/* tasks container */}
         <section className="border-t border-t-gray400 rounded-lg flex justify-center items-center flex-col gap-6 text-gray300 h-[244px]">
           <Image src={clipboard} alt="clipboard" />
-          <p className="leading-[140%] text-center max-w-[366px]">
+          <p className="leading-[140%] text-center max-w-[350px]">
             You don&apos;t have any tasks registered yet Create tasks and
             organize your to-do items
           </p>
