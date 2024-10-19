001e6e50232c17e5f50bacd458eb568abdbb2e8d
benarba mohammed <tewfikhotspur@gmail.com>
2024-10-19 18:25:24 +0100

    delete reducer function and rely on just useState

--- utils/reducer.js
+++
@@ -1,9 +0,0 @@
-export default function reducer(state, action) {
-  const { type, payload } = action;
-  switch (type) {
-    case "ADD_TASK":
-      return { ...state, task: payload };
-    default:
-      return state;
-  }
-}
