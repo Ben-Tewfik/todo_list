6771791387bb8ecca327f7ea2ccd65eb66aefdc3
benarba mohammed <tewfikhotspur@gmail.com>
2024-11-06 21:11:41 +0100

    add margin buttom to add space in the bottom to show logout button

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,7 +2,7 @@ import SingleTask from "./SingleTask";
 
 export default function Tasks({ todos }) {
   return (
-    <section className="flex flex-col gap-3">
+    <section className="flex flex-col gap-3 pb-20">
       {todos.map(todo => {
         return <SingleTask key={todo.id} {...todo} />;
       })}
