d22603f4ba609cb960f7df285acf9c8dda9641ec
benarba mohammed <tewfikhotspur@gmail.com>
2024-11-09 13:39:13 +0100

    access currentUser from contextand add a condition to navigate the user directly to login page if the currentUser is not logged in

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import Todos from "@/components/Todos";
+import { useGlobalContext } from "@/Context/AppContext";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const { currentUser } = useGlobalContext();
+  const router = useRouter();
+  if (!currentUser) {
+    router.push("/login");
+  }
   return <Todos />;
 }
